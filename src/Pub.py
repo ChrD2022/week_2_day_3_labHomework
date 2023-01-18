4f450587ff685f1b48f2c36b09961f901fb5884f
Chris Donnelly <cdonn2020@gmail.com>
2023-01-18 15:59:20 +0000

    MVP

--- src/Pub.py
+++ src/Pub.py
@@ -1,8 +1,19 @@
 class Pub:
-    def __init__(self, name, till):
+    def __init__(self, name, till, drinks):
         self.name = name
         self.till = till
-        self.drinks = []
+        self.drinks = drinks
 
     def increase_till(self, amount):
         self.till += amount
+
+    def find_drink_by_name(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink
+
+    def sell_drink_to_customer(self, name, customer):
+        drink = self.find_drink_by_name(name)
+        customer.decrease_wallet(drink.price)
+        self.increase_till(drink.price)
+        
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,17 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
 
     def setUp(self):
-        self.pub = Pub("The Clansman", 200.00)
+        self.drink_1 = Drink("Lager", 3.50)
+        self.drink_2 = Drink("Whiskey", 4.00)
+
+        drinks = [self.drink_1, self.drink_2]
+        self.pub = Pub("The Clansman", 200.00, drinks)
+
 
     def test_pub_has_name(self):
         self.assertEqual("The Clansman", self.pub.name)
@@ -14,3 +21,13 @@ class TestPub(unittest.TestCase):
         self.pub.increase_till(3.50)
         actual = self.pub.till
         self.assertEqual(expected,actual)
+
+    def test_can_find_drink_by_name(self):
+        drink = self.pub.find_drink_by_name("Lager")
+        self.assertEqual("Lager", drink.name)
+
+    def test_can_sell_drink_to_customer(self):
+        customer = Customer("Dan", 50.00)
+        self.pub.sell_drink_to_customer("Lager", customer)
+        self.assertEqual(46.50, customer.wallet)
+        self.assertEqual(203.50, self.pub.till)
